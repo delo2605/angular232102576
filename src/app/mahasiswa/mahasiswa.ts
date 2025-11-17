f53fe1e6e401cae47c65ccd34c8dc58e3a1c6a8c
febriandhika2332-jpg <febriandhika2332@gmail.com>
2025-11-17 15:13:28 +0700

    new update 15.13

--- src/app/mahasiswa/mahasiswa.ts
+++ src/app/mahasiswa/mahasiswa.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
+import { $locationShim } from '@angular/common/upgrade';
+
+declare const $: any;
 
 @Component({
   selector: 'app-mahasiswa',
@@ -9,6 +12,10 @@ import { Footer } from "../footer/footer";
   templateUrl: './mahasiswa.html',
   styleUrl: './mahasiswa.css',
 })
-export class Mahasiswa {
+export class Mahasiswa implements AfterViewInit{
+  constructor() {}
 
+  ngAfterViewInit(): void {
+    $("#table1").DataTable();
+  }
 }
